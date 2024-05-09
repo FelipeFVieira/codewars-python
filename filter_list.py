8ff4a261b4cdfd73f56b748b8047a12848ad69e5
Felipe França <felipefrancavieira5@gmail.com>
2024-05-09 13:23:30 -0300

    feat: add new Kata - list filtering

---
+++ filter_list.py
@@ -0,0 +1,17 @@
+"""
+Kata description In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+"""
+
+def filter_list(test_list) -> list:
+
+    return [i for i in test_list if isinstance(i, int) and i >= 0]
+
+print(filter_list([1, 5, -2, 0, 'Hello World', '2'])) ## == [1, 5, 0]
+print(filter_list([3, 9, 99.99, "FelipeFVieira", 2])) ## == [3, 9, 2]
+print(filter_list([8,7,20,-100,"CODEWARS","1020"])) ## == [8, 7, 20]
