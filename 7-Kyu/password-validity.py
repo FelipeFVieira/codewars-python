c9b06e64961f4b49699a6877220d2a9601f6faf5
Felipe de França Vieira <felipefrancavieira5@gmail.com>
2025-01-18 01:00:09 -0300

    docs: added kata description for password-validity.py

--- 7-Kyu/password-validity.py
+++ 7-Kyu/password-validity.py
@@ -1,3 +1,19 @@
+"""
+Your job is to create a simple password validation function, as seen on many websites.
+
+The rules for a valid password are as follows:
+
+There needs to be at least 1 uppercase letter.
+There needs to be at least 1 lowercase letter.
+There needs to be at least 1 number.
+The password needs to be at least 8 characters long.
+You are permitted to use any methods to validate the password.
+
+Kata Level: 7 Kyu
+
+Link: https://www.codewars.com/kata/56a921fa8c5167d8e7000053
+"""
+
 def password_validity(password):
     # Check the length of the password
     if len(password) < 8:
